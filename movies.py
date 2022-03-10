0778e709d85f84601c829af56860a5c6cf0315f2
matthew-viglione <19675897+matthew-viglione@users.noreply.github.com>
2022-03-10 13:20:26 -0700

    Add debugging and format with black

--- movies.py
+++ movies.py
@@ -2,6 +2,7 @@
 from json import dumps
 import logging
 import os
+import sys
 
 from flask import (
     Flask,
@@ -15,13 +16,18 @@ from neo4j import (
 )
 
 
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logging.getLogger("neo4j").addHandler(handler)
+logging.getLogger("neo4j").setLevel(logging.DEBUG)
+
 app = Flask(__name__, static_url_path="/static/")
 
-url = os.getenv("NEO4J_URI", "neo4j+s://demo.neo4jlabs.com")
-username = os.getenv("NEO4J_USER", "movies")
-password = os.getenv("NEO4J_PASSWORD", "movies")
+url = os.getenv("NEO4J_URI", "neo4j://localhost")
+username = os.getenv("NEO4J_USER", "neo4j")
+password = os.getenv("NEO4J_PASSWORD", "p")
 neo4j_version = os.getenv("NEO4J_VERSION", "4")
-database = os.getenv("NEO4J_DATABASE", "movies")
+database = os.getenv("NEO4J_DATABASE", "neo4j")
 
 port = os.getenv("PORT", 8080)
 
@@ -57,27 +63,25 @@ def serialize_movie(movie):
         "duration": movie["duration"],
         "rated": movie["rated"],
         "tagline": movie["tagline"],
-        "votes": movie.get("votes", 0)
+        "votes": movie.get("votes", 0),
     }
 
 
 def serialize_cast(cast):
-    return {
-        "name": cast[0],
-        "job": cast[1],
-        "role": cast[2]
-    }
+    return {"name": cast[0], "job": cast[1], "role": cast[2]}
 
 
 @app.route("/graph")
 def get_graph():
     def work(tx, limit):
-        return list(tx.run(
-            "MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
-            "RETURN m.title AS movie, collect(a.name) AS cast "
-            "LIMIT $limit",
-            {"limit": limit}
-        ))
+        return list(
+            tx.run(
+                "MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
+                "RETURN m.title AS movie, collect(a.name) AS cast "
+                "LIMIT $limit",
+                {"limit": limit},
+            )
+        )
 
     db = get_db()
     results = db.read_transaction(work, request.args.get("limit", 100))
@@ -97,19 +101,20 @@ def get_graph():
                 source = i
                 i += 1
             rels.append({"source": source, "target": target})
-    return Response(dumps({"nodes": nodes, "links": rels}),
-                    mimetype="application/json")
+    return Response(dumps({"nodes": nodes, "links": rels}), mimetype="application/json")
 
 
 @app.route("/search")
 def get_search():
     def work(tx, q_):
-        return list(tx.run(
-            "MATCH (movie:Movie) "
-            "WHERE toLower(movie.title) CONTAINS toLower($title) "
-            "RETURN movie",
-            {"title": q_}
-        ))
+        return list(
+            tx.run(
+                "MATCH (movie:Movie) "
+                "WHERE toLower(movie.title) CONTAINS toLower($title) "
+                "RETURN movie",
+                {"title": q_},
+            )
+        )
 
     try:
         q = request.args["q"]
@@ -120,7 +125,7 @@ def get_search():
         results = db.read_transaction(work, q)
         return Response(
             dumps([serialize_movie(record["movie"]) for record in results]),
-            mimetype="application/json"
+            mimetype="application/json",
         )
 
 
@@ -134,25 +139,29 @@ def get_movie(title):
             "COLLECT([person.name, "
             "HEAD(SPLIT(TOLOWER(TYPE(r)), '_')), r.roles]) AS cast "
             "LIMIT 1",
-            {"title": title_}
+            {"title": title_},
         ).single()
 
     db = get_db()
     result = db.read_transaction(work, title)
 
-    return Response(dumps({"title": result["title"],
-                           "cast": [serialize_cast(member)
-                                    for member in result["cast"]]}),
-                    mimetype="application/json")
+    return Response(
+        dumps(
+            {
+                "title": result["title"],
+                "cast": [serialize_cast(member) for member in result["cast"]],
+            }
+        ),
+        mimetype="application/json",
+    )
 
 
 @app.route("/movie/<title>/vote", methods=["POST"])
 def vote_in_movie(title):
     def work(tx, title_):
         return tx.run(
-            "MATCH (m:Movie {title: $title}) "
-            "SET m.votes = coalesce(m.votes, 0) + 1;",
-            {"title": title_}
+            "MATCH (m:Movie {title: $title}) " "SET m.votes = coalesce(m.votes, 0) + 1;",
+            {"title": title_},
         ).consume()
 
     db = get_db()
